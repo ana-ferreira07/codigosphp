6e88adcbc1796cd1f5958b7ac1214e042b3ec28c
ANA BEATRIZ FERREIRA PASSOS <passos.2967@aluno.pr.senac.br>
2023-03-06 08:41:34 -0300

    codigos

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+    $nome = "Ana";
+    $idade = "16";
+    $endereco = "R. Jose bonifacio";
+    $cidade = "Ivaipora";
+    $cep = "86870000";
+    $semestre1 = 9;
+    $semestre2 = 9;
+    $semestre3 = 10;
+    $media = ($semestre1+$semestre2+$semestre3)/3;
+
+    echo "seu nome é:" . $nome;
+    echo("</br>");
+    echo "sua idade é:" . $idade;
+    echo("</br>");
+    echo "seu endereco:" . $endereco;
+    echo("</br>");
+    echo "sua cidade é:" . $cidade;
+    echo("</br>");
+    echo "seu cep é:" . $cep;
+    echo("</br>");
+    echo "sua nota no semestre1 é:" . $semestre1;
+    echo("</br>");
+    echo "sua nota no semestre2:" . $semestre2;
+    echo("</br>");
+    echo "sua nota no semestre3:" . $semestre3;
+    echo("</br>");
+    echo "sua média é:" . $media;
+?>
